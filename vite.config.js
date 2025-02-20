34d20c2f5e83e6731d9dbe71d82dce0e5f2c51af
Linus Schulze <linuscyber146@gmail.com>
2025-02-20 10:32:52 +0100

    vite config

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    root: './src', // Set the source directory
-    build: {
-        outDir: '../dist', // The directory where the build output will be placed
-        sourcemap: true,  // Enable sourcemaps for debugging
-    },
-});
